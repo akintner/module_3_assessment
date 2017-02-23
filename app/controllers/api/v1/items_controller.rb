6b1ef742eedcd59e5d69a1252429d749a96c434a
Amz K <akintner@u.rochester.edu>
2017-02-23 09:09:41 -0700

    add routes for API and set up controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,25 +1,3 @@
 class Api::V1::ItemsController < ApplicationController
-  # def index
-  #   render json: Item.all
-  # end
-
-  # def show
-  #   render json: Item.find(params[:id])
-  # end
-
-  # def destroy
-  #   item = Item.find(params[:id])
-  #   render json: item.destroy
-  # end
-
-  # def create
-  #   item = Item.create(item_params)
-  #   render json: item
-  # end
-
-  # private
-
-  #   def item_params
-  #     params.require(:item).permit(:name, :description, :image_url)
-  #   end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  get "/search", to: "search#index"
-  resources :stores, only: [:show]
-
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
   namespace :api do 
     namespace :v1 do 
-      resources :items
+      get "/items", to: "items#index"
+      get "/items/:id", to: "items#show"
+      get "/items/new", to: "items#new"
+      post "/items", to: "items#create"
+      delete "/items/:id", to: "items#delete"
     end
   end
 end
