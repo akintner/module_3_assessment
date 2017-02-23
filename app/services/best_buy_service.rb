2284083f6c305db48784f634e88002e90847a0f8
Amz K <akintner@u.rochester.edu>
2017-02-23 11:49:49 -0700

    make view for stores coming through

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -4,7 +4,7 @@ class BestBuyService
   end
 
   def self.nearest_to_zip(zip)
-    response = Faraday.get("http://api.bestbuy.com/v1/stores?format=json&postalCode=#{zip}&show=phone,storeId,city,distance,storeType,longName&apiKey=#{@api_key}")
+    response = Faraday.get("http://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&pageSize=1&apiKey=krmeujcx6hn477xn5gma4pmn")
     JSON.parse(response.body, symbolize_names: true)
     byebug
   end
